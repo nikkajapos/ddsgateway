eb88f822cd71236392d4949e563ed4f018a0d08f
nikkajapos <japosnikka03@gmail.com>
2025-04-11 14:53:33 +0800

    updated bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -81,7 +81,7 @@ $app->routeMiddleware([
    $app->register(App\Providers\AuthServiceProvider::class);
    $app->register(Laravel\Passport\PassportServiceProvider::class);
    $app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
-   $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
+   //$app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 
    //$app->register(App\Providers\EventServiceProvider::class);
 
